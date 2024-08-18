b3f1d51312192832c9364a2eb26cf6df48d77c99
rahulgarg55 <gargr010>
2024-08-18 21:48:20 +0530

    card

--- food_app/src/screens/Home.js
+++ food_app/src/screens/Home.js
@@ -11,7 +11,8 @@ export default function Home() {
           <img src="..." className="card-img-top" alt="Card image" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text">This is Some Important text.</p>
+            <div className ='container'></div>
           </div>
         </div>
       </div>
