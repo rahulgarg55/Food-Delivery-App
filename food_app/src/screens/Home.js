9bcd8e0b87247a92e5d454574a4a4a4189cf2826
rahulgarg55 <gargr010>
2024-08-18 21:55:22 +0530

    card

--- food_app/src/screens/Home.js
+++ food_app/src/screens/Home.js
@@ -8,15 +8,21 @@ export default function Home() {
       <div><Navbar/></div>
       <div>
         <div className="card mt-3" style={{"width": "18rem", "maxHeight":"360px"}}>
-          <img src="..." className="card-img-top" alt="Card image" />
+          <img src="..." className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">This is Some Important text.</p>
-            <div className ='container'></div>
+            <div className ='container w-100'>
+            <select className='m-2 h-100 w-100 bg-success'>
+            {Array.from(Array(6), (i)=>{
+              return <option key = {i+1} value={i+1}>{i+1}</option>
+            })}
+            </select>
           </div>
         </div>
       </div>
       <div><Footer/></div>
+    </div>
     </div>
   )
 }
